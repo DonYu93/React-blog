f55c19665577e1b869352d6123498ac13bb51427
Don <don.dongjun.yu@gmail.com>
2020-09-12 18:59:34 -0400

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+const { User } = require("./models/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://DonYu:abcd1234@react-blog.smgig.mongodb.net/react-blog?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.json({ "hello~": "Hi~~ asdfasdfa" });
+});
+
+app.post("/api/user/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+      userData: doc,
+    });
+  });
 });
 
 app.listen(5000);
